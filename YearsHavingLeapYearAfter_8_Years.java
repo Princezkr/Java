862924a1914f68f8b3eb9dc53a11103a2cc0f78d
Karmajoty Biswal <150656048+Princezkr@users.noreply.github.com>
2026-01-13 19:14:19 +0530

    Leap Years After 8 Years
    This Shows The Leap Year That Came After 8 years instead of 4 years .

--- YearsHavingLeapYearAfter_8_Years.java
+++ YearsHavingLeapYearAfter_8_Years.java
@@ -10,12 +10,7 @@ public class YearsHavingLeapYearAfter_8_Years{
         System.out.println("The Years Having Leap Year After 8 Years Are:");
         for(int i=Start_Year; i<=End_Year; i++){
             if(i%4==0 && i%100==0 && i%400!=0){
-                if(i+4>End_Year){
-                    break;
-                }
-                else{
-                    System.out.println("Year Having Leap Year After 8 years: " + (i+4));
-                }
+                System.out.println((i-4) + " And " + (i+4));
             }
         }
         sc.close();
