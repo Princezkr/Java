fa5edd725889f6a3bbd48f381fa5fb1d1cefd285
Karmajoty Biswal <150656048+Princezkr@users.noreply.github.com>
2026-01-13 19:14:43 +0530

    Factorial Of A num
    This Give Factorial Of A Specified Number Using java

--- Factorial.java
+++ Factorial.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 public class Factorial{
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter a positive integer N: ");
-        int N=sc.nextInt();
-        int Factorial=1;
-        for(int i=1;i<=N;i++){
-            Factorial=Factorial*i;
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a positive integer N: ");
+            int N=sc.nextInt();
+            int Factorial=1;
+            for(int i=1;i<=N;i++){
+                Factorial=Factorial*i;
+            }
+            System.out.println("Factorial Of "+N+" is " + Factorial);
         }
-        System.out.println("Factorial Of "+N+" is " + Factorial);
-        sc.close();
     }
-}
